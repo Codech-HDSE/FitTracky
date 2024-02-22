8a757ff2e36c58b50484ee2c9b81b6a1a21285b4
ahamedmusanik98 <139645056+ahamedmusanik98@users.noreply.github.com>
2024-02-22 22:20:16 +0530

    update

---
+++ FitTracky/app/src/main/java/com/example/fittracky/ShouldersActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fittracky
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShouldersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shoulders)
+    }
+}
