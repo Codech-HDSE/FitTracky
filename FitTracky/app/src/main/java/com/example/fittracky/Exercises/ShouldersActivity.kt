a7a42ce1dbe2ef82d31bc00aeb4cd12537c94a72
ahamedmusanik98 <139645056+ahamedmusanik98@users.noreply.github.com>
2024-02-22 22:38:45 +0530

    update

--- FitTracky/app/src/main/java/com/example/fittracky/Exercises/ShouldersActivity.kt
+++ FitTracky/app/src/main/java/com/example/fittracky/Exercises/ShouldersActivity.kt
@@ -2,11 +2,17 @@ package com.example.fittracky.Exercises
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.example.fittracky.R
 
 class ShouldersActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shoulders)
+
+        val backButton = findViewById<ImageButton>(R.id.back_button)
+        backButton.setOnClickListener {
+            onBackPressed() // This will perform the default back action (e.g., navigate back)
+        }
     }
 }
